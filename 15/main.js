23275f59d29cfffe543ddfd14dfe8bd7afdd56d2
Mick Enev <menev2@illinois.edu>
2025-02-16 19:23:47 -0600

    15

---
+++ 15/main.js
@@ -0,0 +1,21 @@
+function isPrime(number) {
+    if (number === 2) {
+        return true;
+    }
+    if (number < 2) {
+        return false;
+    }
+
+    let maxDiv = Math.sqrt(number);
+
+    for (let i = 2; i <= maxDiv; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+for (let i = 0; i < 20; i++) {
+    console.log("The Number " + i + " is prime? " + isPrime(i));
+}

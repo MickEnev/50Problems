768778c09b60da06c25279176c0874d7917d43a2
Mick Enev <menev2@illinois.edu>
2025-02-17 08:54:04 -0600

    29

---
+++ 29/main.js
@@ -0,0 +1,6 @@
+function addTwoStrings(string1, string2) {
+    let res = parseInt(string1) + parseInt(string2);
+    return res.toString();
+}
+
+console.log(addTwoStrings("2909034221912398942349", "1290923909029309499"));
